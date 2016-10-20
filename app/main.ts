be9eb2112fcf49493a2d4e2e7e7249e6510c6b84
Mark Jones <mark@getkahoot.com>
2016-10-20 22:01:14 +0100

    Filter the comics so that it only includes comics that cost less than 4 dollars

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,5 @@
 import { Observable } from 'rxjs';
-import { prop, filter, compose, gt } from 'ramda';
+import { prop, filter, compose, gt, nth } from 'ramda';
 
 const log = (value) => {
   console.log(value);
@@ -9,7 +9,13 @@ const log = (value) => {
 const getResults = prop('results');
 const getData = prop('data');
 const getPageCount = prop('pageCount');
+const getPrice = prop('price');
+const getPrices = prop('prices');
+
+const getFirst = nth(0);
+
 const lt100 = gt(100);
+const lt4 = gt(4);
 
 const apiKey = 'fc67721c305c84f50f7c6646c9b8d9d0';
 const apiUrl = `http://gateway.marvel.com/v1/public/comics?apikey=${apiKey}`;
@@ -22,10 +28,15 @@ const response$ =
     .flatMap((response) => Observable.fromPromise(response.json()));
 
 const getComicsFromResponse = compose(getResults, getData);
+const getComicPrice = compose(getPrice, getFirst, getPrices);
 const getLt100Pages = filter(compose(lt100, getPageCount));
+const getLt4Dollars = filter(compose(lt4, getComicPrice));
 
 const comics$ = response$.map(getComicsFromResponse)
 
-const filteredComics$ = comics$.map(getLt100Pages);
+const filteredComics$ =
+  comics$
+    .map(getLt100Pages)
+    .map(getLt4Dollars);
 
 filteredComics$.subscribe((comics) => console.log(comics));
