da4cc765a9aa26aaa8e1c9826e257d8583118a06
Mark Jones <mark@getkahoot.com>
2016-10-20 21:51:16 +0100

    Filter the comics so that it only includes comics with less than 100 pages

--- app/main.ts
+++ app/main.ts
@@ -1,9 +1,15 @@
 import { Observable } from 'rxjs';
+import { prop, filter, compose, gt } from 'ramda';
 
 const log = (value) => {
   console.log(value);
   return value;
 };
+
+const getResults = prop('results');
+const getData = prop('data');
+const getPageCount = prop('pageCount');
+const lt100 = gt(100);
 
 const apiKey = 'fc67721c305c84f50f7c6646c9b8d9d0';
 const apiUrl = `http://gateway.marvel.com/v1/public/comics?apikey=${apiKey}`;
@@ -15,4 +21,11 @@ const response$ =
     .flatMap(() => Observable.fromPromise(fetch(apiUrl)))
     .flatMap((response) => Observable.fromPromise(response.json()));
 
-response$.subscribe((response) => console.log(response));
+const getComicsFromResponse = compose(getResults, getData);
+const getLt100Pages = filter(compose(lt100, getPageCount));
+
+const filteredComics$ =
+  response$.map(getComicsFromResponse)
+    .map(getLt100Pages);
+
+filteredComics$.subscribe((comics) => console.log(comics));
