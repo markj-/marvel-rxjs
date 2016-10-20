c50ff1cb2c5c6c09dbf449d1e87eb740c41fe740
Mark Jones <mark@getkahoot.com>
2016-10-20 21:28:34 +0100

    Get the json

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,13 @@
 import { Observable } from 'rxjs';
 
-Observable.of('Hello world')
-  .subscribe(console.log);
+const apiKey = 'fc67721c305c84f50f7c6646c9b8d9d0';
+const apiUrl = `http://gateway.marvel.com/v1/public/comics?apikey=${apiKey}`;
+
+const request$ = Observable.of(apiUrl);
+
+const response$ =
+  request$
+    .flatMap(() => Observable.fromPromise(fetch(apiUrl)))
+    .flatMap((response) => Observable.fromPromise(response.json()));
+
+response$.subscribe((response) => console.log(response));
