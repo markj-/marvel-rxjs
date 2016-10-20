f9bd0634fb97f5e975c45964cbb2987ae3479d73
Mark Jones <mark@getkahoot.com>
2016-10-20 21:46:16 +0100

    Add a logging utility function

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,10 @@
 import { Observable } from 'rxjs';
 
+const log = (value) => {
+  console.log(value);
+  return value;
+};
+
 const apiKey = 'fc67721c305c84f50f7c6646c9b8d9d0';
 const apiUrl = `http://gateway.marvel.com/v1/public/comics?apikey=${apiKey}`;
 
